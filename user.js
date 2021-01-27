9a7266923ca6602163b1211488825c71e8b51b83
Damien Saavi <damiensaavi@gmail.com>
2021-01-27 13:57:45 -0800

    set schema type to mixed for ingredients and timer removed redundant connection call

--- user.js
+++ user.js
@@ -3,32 +3,29 @@ const passportLocalMongoose = require('passport-local-mongoose')
 const Schema = mongoose.Schema
 
 const Step = new Schema({
-    instruction: String,
-    ingredients: [{
-        name:String, amount:Number, unit:String
-    }],
-    timer: {
-        name:String,
-        duration:{minutes:Number, seconds:Number},
-        synced:Boolean}
+  instruction: String,
+  ingredients: [Schema.Types.Mixed],
+  // [name: String, amount: Number, unit: String]
+  timer: Schema.Types.Mixed
+  // name:String,
+  // duration:{minutes:Number, seconds:Number},
+  // synced:Boolean}
 })
 
 const Recipe = new Schema({
-    name: String,
-    ingredients: [{name:String, amount:Number, unit:String}],
-    steps: [Step]
+  name: String,
+  ingredients: [{ name: String, amount: Number, unit: String }],
+  steps: [Step]
 })
 
 const User = new Schema({
-  username: {type:String, required:true, unique:true},
-  name: {type:String, required:true},
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  recipes: [Recipe]
 
 })
 
-mongoose.connect('mongodb://localhost/brewmate',
-  { useNewUrlParser: true, useUnifiedTopology: true })
-
-User.plugin(passportLocalMongoose, {usernameField: 'email'})
+User.plugin(passportLocalMongoose, { usernameField: 'email' })
 const Users = mongoose.model('user', User)
 
 module.exports = Users
